94ed08d93ed054529fefbdc328e33cff5e34fb5b
tae2089 <tae2089002@gmail.com>
2023-05-05 22:27:41 +0900

    feature: :seedling: create rootcmd nameing bungee (#1)

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/spf13/cobra"
 )
 
@@ -19,21 +18,15 @@ var (
 		Short: `Bungee is a CLI tool that is related to server connection. It supports AWS SSM connection, GCP IAP tunneling, SSH, and more.`,
 		Long:  `Bungee is a CLI tool that is related to server connection. It supports AWS SSM connection, GCP IAP tunneling, SSH, and more.`,
 	}
-
-	_version                 string
-	_credential              *Credential
-	_credentialWithMFA       = fmt.Sprintf("%s_mfa", config.DefaultSharedCredentialsFilename())
-	_credentialWithTemporary = fmt.Sprintf("%s_temporary", config.DefaultSharedCredentialsFilename())
 )
-
-type Credential struct {
-	awsProfile    string
-	awsConfig     *aws.Config
-	gossmHomePath string
-	ssmPluginPath string
-}
 
 func init() {
 	// cobra.OnInitialize(initConfig)
+}
 
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
