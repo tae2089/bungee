f1c7bd05a8f313a556be2400a375c4761fbe820e
tae2089 <tae2089002@gmail.com>
2023-05-05 22:28:46 +0900

    feature: :seedling: create list subcommand in aws for getting ec2 list (#1)

--- cmd/ec2_list.go
+++ cmd/ec2_list.go
@@ -2,27 +2,24 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"github.com/tae2089/bungee/internal/di"
 )
 
 var (
 	// rootCmd represents the base command when called without any sub-commands
 	ec2ListCmd = &cobra.Command{
-		Use:   "aws",
+		Use:   "list",
 		Short: `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
 		Long:  `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
-		Run:   func(cmd *cobra.Command, args []string) {},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			awsService := di.InitAwsService("", "")
+			return awsService.GetEc2List()
+			// awsService.ListInstances()
+		},
 	}
 )
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,	will be global for your application.
-	// Cobra also supports local flags, which will only run when this action is called directly.
-	ec2ListCmd.Flags().StringP("profile", "p", "", `[optional] if you are having multiple aws profiles, it is one of profiles (default is AWS_PROFILE environment variable or default)`)
-	ec2ListCmd.Flags().StringP("region", "r", "", `[optional] it is region in AWS that would like to do something`)
-	// mapping viper
-	viper.BindPFlag("profile", ec2ListCmd.PersistentFlags().Lookup("profile"))
-	viper.BindPFlag("region", ec2ListCmd.PersistentFlags().Lookup("region"))
+
 	awsCmd.AddCommand(ec2ListCmd)
 }
