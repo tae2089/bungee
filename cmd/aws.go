d462f3c0bdc4a48127fc8e56001263fc3b07c079
taebin lim <tae2089002@gmail.com>
2023-05-06 09:05:20 +0900

    Revert "feature: :seedling: create aws subcommand for aws features (#1)"
    This reverts commit 0b781fac871eaca0a112accb0c3ce5ad0d98f895.

--- cmd/aws.go
+++ cmd/aws.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var (
@@ -12,25 +10,11 @@ var (
 		Use:   "aws",
 		Short: `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
 		Long:  `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
+		Run:   func(cmd *cobra.Command, args []string) {},
 	}
-	_credential *Credential
 )
 
-type Credential struct {
-	awsProfile    string
-	awsConfig     *aws.Config
-	gossmHomePath string
-	ssmPluginPath string
-}
-
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,	will be global for your application.
-	// Cobra also supports local flags, which will only run when this action is called directly.
-	ec2ListCmd.PersistentFlags().StringP("profile", "p", "", `[optional] if you are having multiple aws profiles, it is one of profiles (default is AWS_PROFILE environment variable or default)`)
-	ec2ListCmd.PersistentFlags().StringP("region", "r", "", `[optional] it is region in AWS that would like to do something`)
-	// mapping viper
-	viper.BindPFlag("profile", ec2ListCmd.PersistentFlags().Lookup("profile"))
-	viper.BindPFlag("region", ec2ListCmd.PersistentFlags().Lookup("region"))
+
 	rootCmd.AddCommand(awsCmd)
 }
