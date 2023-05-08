ed09f5c9910c2dffdd75550a9ebf1ffcfbb12423
bob(taebinlim) <bob@bc-labs.net>
2023-05-08 09:06:21 +0900

    feature: :seedling: add stop subcommand for ec2 instance (#7)

---
+++ cmd/ec2_stop.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tae2089/bungee/internal/di"
+)
+
+var (
+	// rootCmd represents the base command when called without any sub-commands
+	ec2StopCmd = &cobra.Command{
+		Use:   "stop",
+		Short: `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
+		Long:  `The aws subcommand includes functionalities such as instance listing, SSM connection, and deletion.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			profile := strings.TrimSpace(viper.GetString("profile"))
+			region := strings.TrimSpace(viper.GetString("region"))
+			awsService := di.InitAwsService(profile, region)
+			return awsService.StartInstance(args)
+			// awsService.ListInstances()
+		},
+	}
+)
+
+func init() {
+	awsCmd.AddCommand(ec2StopCmd)
+}
