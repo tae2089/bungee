e7ebb4aaa3f32ee5c3b215e91c97eb3399b51851
tae2089 <tae2089002@gmail.com>
2023-05-11 21:30:06 +0900

    feature: :seedling: register ssh info for db (#8)

--- internal/ssh/ssh_service_impl.go
+++ internal/ssh/ssh_service_impl.go
@@ -1,5 +1,23 @@
 package ssh
 
-type sshServiceImpl struct{}
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type sshServiceImpl struct {
+	db *sql.DB
+}
 
 var _ SSHService = (*sshServiceImpl)(nil)
+
+// RegisterSSHInfo implements SSHService
+func (ssh *sshServiceImpl) RegisterSSHInfo(s SshRegisterDto) error {
+	// insert data
+	_, err := ssh.db.Exec("INSERT INTO ssh_info (name, user, host, port) VALUES (?,?,?,?)", s.Key, s.User, s.Host, s.Port)
+	if err != nil {
+		return err
+	}
+	return nil
+}
