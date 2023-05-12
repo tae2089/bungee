9ee0881fe98437f4ed733e27b8c54d98ba799161
tae2089 <tae2089002@gmail.com>
2023-05-12 22:17:24 +0900

    fix: :fire: change db close to idle conns 0 (#8)

--- internal/config/db.go
+++ internal/config/db.go
@@ -4,11 +4,11 @@ import "database/sql"
 
 func NewDbConfig() (*sql.DB, error) {
 	// open sqlite
-	db, err := sql.Open("sqlite3", "~/bungee.db")
+	db, err := sql.Open("sqlite3", "bungee.db")
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
+	defer db.SetMaxIdleConns(0)
 
 	//create table if not exists
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS ssh_info (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL UNIQUE, user VARCHAR(255) NOT NULL UNIQUE, host VARCHAR(255) NOT NULL UNIQUE, port INTEGER NOT NULL DEFAULT 22)")
