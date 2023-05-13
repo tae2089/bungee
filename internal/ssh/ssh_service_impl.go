8294f5acf0587027b64022b94f9d8da3dc2b7358
tae2089 <tae2089002@gmail.com>
2023-05-13 15:18:48 +0900

    feature: :seedling: get sshinfo list (#17)

--- internal/ssh/ssh_service_impl.go
+++ internal/ssh/ssh_service_impl.go
@@ -2,8 +2,11 @@ package ssh
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/olekukonko/tablewriter"
 )
 
 type sshServiceImpl struct {
@@ -13,12 +16,37 @@ type sshServiceImpl struct {
 var _ SSHService = (*sshServiceImpl)(nil)
 
 // RegisterSSHInfo implements SSHService
-func (ssh *sshServiceImpl) RegisterSSHInfo(s *SshRegisterDto) error {
+func (ssh *sshServiceImpl) RegisterSSHInfo(s *SshRegisterDto, privateKey string) error {
 	// insert data
 	defer ssh.db.Close()
-	_, err := ssh.db.Exec("INSERT INTO ssh_info (name, user, host, port) VALUES (?,?,?,?)", s.Name, s.User, s.Host, s.Port)
+	_, err := ssh.db.Exec("INSERT INTO ssh_info (name, user, host, port, key, password) VALUES (?,?,?,?,?,?)", s.Name, s.User, s.Host, s.Port, privateKey, s.Password)
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+// ShowSSHInfoList implements SSHService
+func (ssh *sshServiceImpl) ShowSSHInfoList() error {
+	// select data
+	defer ssh.db.Close()
+	rows, err := ssh.db.Query("SELECT name, user, host, port, password FROM ssh_info")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "User", "Host", "Port", "Password"})
+	for rows.Next() {
+		var s SshRegisterDto
+		err := rows.Scan(&s.Name, &s.User, &s.Host, &s.Port, &s.Password)
+		if err != nil {
+			return err
+		}
+		table.Append([]string{
+			s.Name, s.User, s.Host, fmt.Sprint(s.Port), s.Password,
+		})
+	}
+	table.Render()
 	return nil
 }
