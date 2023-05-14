3f070d0b7a2a202788cd18fffb43f0d3b78258c7
tae2089 <tae2089002@gmail.com>
2023-05-14 11:41:44 +0900

    feature: :seedling: create Ssm Session Start (#12)

--- internal/aws/aws_service.go
+++ internal/aws/aws_service.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 var (
@@ -15,13 +16,15 @@ type AwsServie interface {
 	GetEc2List() error
 	StartInstance(instanceId []string) error
 	StopInstances(instanceId []string) error
+	ConnectInstances(instanceId string) error
 }
 
-func NewAwsService(client *ec2.Client) AwsServie {
+func NewAwsService(client *ec2.Client, ssmClient *ssm.Client) AwsServie {
 	if awsService == nil {
 		awsServiceSync.Do(func() {
 			awsService = &awsServiceImpl{
 				client: client,
+				ssm:    ssmClient,
 			}
 		})
 	}
--- internal/aws/aws_service_impl.go
+++ internal/aws/aws_service_impl.go
@@ -2,11 +2,17 @@ package aws
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/smithy-go"
 	"github.com/olekukonko/tablewriter"
 )
 
@@ -14,6 +20,7 @@ var _ AwsServie = (*awsServiceImpl)(nil)
 
 type awsServiceImpl struct {
 	client *ec2.Client
+	ssm    *ssm.Client
 }
 
 // StartInstance implements AwsServie
@@ -79,5 +86,86 @@ func (a *awsServiceImpl) GetEc2List() error {
 
 	table.Render()
 
+	return nil
+}
+
+// ConnectInstances implements AwsServie
+func (a *awsServiceImpl) ConnectInstances(instanceId string) error {
+	// AWS 세션 생성
+
+	// Session Manager를 통해 EC2 인스턴스에 연결
+	// 세션 시작 요청
+	input := &ssm.StartSessionInput{
+		Target: aws.String(instanceId),
+	}
+	startSessionOutput, err := a.ssm.StartSession(context.TODO(), input)
+	// sessJson, err := json.Marshal(startSessionOutput)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// paramsJson, err := json.Marshal(input)
+	// if err != nil {
+	// 	return err
+	// }
+	if err != nil {
+		var apiErr smithy.APIError
+		if errors.As(err, &apiErr) {
+			if apiErr.ErrorCode() == "TargetNotConnected" {
+				return fmt.Errorf("failed to start session: target is not connected or does not exist")
+			}
+		}
+		return err
+	}
+
+	// 세션 ID와 세션 토큰 얻기
+	sessionID := *startSessionOutput.SessionId
+	// sessionToken := *startSessionOutput.Token
+
+	// 세션 터미널 입출력 처리
+	err = callProcess("")
+	if err != nil {
+		return err
+	}
+
+	// 세션 종료 요청
+	_, err = a.ssm.TerminateSession(context.TODO(), &ssm.TerminateSessionInput{
+		SessionId: aws.String(sessionID),
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Session terminated")
+
+	return nil
+}
+
+// CallProcess calls process.
+func callProcess(process string, args ...string) error {
+	call := exec.Command(process, args...)
+	call.Stderr = os.Stderr
+	call.Stdout = os.Stdout
+	call.Stdin = os.Stdin
+
+	// ignore signal(sigint)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+	done := make(chan bool, 1)
+	go func() {
+		for {
+			select {
+			case <-sigs:
+			case <-done:
+				break
+			}
+		}
+	}()
+	defer close(done)
+
+	// run subprocess
+	if err := call.Run(); err != nil {
+		return err
+	}
 	return nil
 }
